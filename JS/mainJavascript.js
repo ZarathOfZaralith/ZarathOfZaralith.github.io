fbc221abc35c5a1ec27d96d36775efa6b48bdd66
ZarathOfZaralith <41802144+ZarathOfZaralith@users.noreply.github.com>
2018-07-29 15:02:33 -0400

    Add files via upload

---
+++ JS/mainJavascript.js
@@ -0,0 +1,7 @@
+//This is just the javascript file that will be used to navigate the content of the site. 
+
+function Menu()
+{
+	var Nav = document.querySelector('.sideNav');
+	console.log("boop");
+}
